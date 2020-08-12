bb69f862316f92eaeea1cc0be187b5cae88d4f94
Banks Nussman <banks@nussman.us>
2020-08-11 21:23:31 -0400

    edit profile now supports warning

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -62,7 +62,17 @@ function EditProfile() {
         <BeepAppBar/>
             <div className="lg:container px-4 mx-auto">
                 {status && 
-                    <div role="alert" className={status.status === "success" ? "bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" : "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" }>
+                    <div
+                        role="alert"
+                        className={status.status === "success" ?
+                            "bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4"
+                            :
+                            status.status === "warning" ?
+                                "bg-orange-100 border border-orange-400 text-orange-700 px-4 py-3 rounded relative mb-4"
+                                :
+                                "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4"
+                        }
+                    >
                         {status.message}
                     </div>
                 }
