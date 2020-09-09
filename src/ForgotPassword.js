fafb9fcf9b0b85b67586eb48eb61f5cd44ad96d4
Banks Nussman <banks@nussman.us>
2020-09-09 13:06:34 -0400

    fixed forgot password alert

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -43,11 +43,25 @@ function ForgotPassword() {
             <div className="lg:container px-4 mx-auto">
             {status && 
                 <div role="alert" className="mb-4" onClick={() => setStatus(null)}>
-                    <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
-                        Forgot Password Error
+                    <div className={status.status === "success" ?
+                            "bg-green-500 text-white font-bold rounded-t px-4 py-2"
+                            :
+                            status.status === "warning" ?
+                            "bg-yellow-500 text-white font-bold rounded-t px-4 py-2"
+                            :
+                            "bg-red-500 text-white font-bold rounded-t px-4 py-2"
+                        }>
+                        Edit profile {status.status}
                     </div>
-                    <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
-                        <Error error={status.message} />
+                    <div className={status.status === "success" ?
+                            "border border-t-0 border-green-400 rounded-b bg-green-100 px-4 py-3 text-green-700"
+                            :
+                            status.status === "warning" ?
+                            "border border-t-0 border-yellow-400 rounded-b bg-yellow-100 px-4 py-3 text-yellow-700"
+                            :
+                            "border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700"
+                        }>
+                        <Error error={status.message}/>
                     </div>
                 </div>
             }
