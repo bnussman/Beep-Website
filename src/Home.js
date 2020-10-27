f0830b0f306c7ca7c37441d24376441fac676f57
Banks Nussman <banks@nussman.us>
2020-10-26 21:38:21 -0400

    added iOS and Android download buttons and fixed navbar margin

--- src/Home.js
+++ src/Home.js
@@ -19,6 +19,21 @@ function Home() {
                     </div>
                 </div>
 
+
+
+                <div className="rounded overflow-hidden shadow-md mb-4">
+                    <div className="px-6 py-4">
+                        <div className="flex flex-row ml-2 justify-center">
+                            <a target="_blank" rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=app.ridebeep.App">
+                                <img style={{height: "51px"}} className="m-1" alt="Download on Google Play" src="/google.png"/>
+                            </a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://apps.apple.com/us/app/ride-beep-app/id1528601773">
+                                <img style={{height: "50px"}} className="m-1" alt="Download on The App Store" src="/apple.svg"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="rounded shadow-md mb-4">
                     <div className="flex flex-wrap justify-center">
                         <img style={{width: "250px", height: "500px"}} className="mt-5 mb-5" src="/ios.png" alt="iOS Example" />
@@ -30,11 +45,5 @@ function Home() {
 
 }
 
-/*
-                    <div className="flex flex-row ml-2">
-                        <img style={{height: "51px"}} className="m-1" alt="Download on Google Play" src="/google.png"/>
-                        <img style={{height: "50px"}} className="m-1" alt="Download on The App Store" src="/apple.svg"/>
-                    </div>
-*/
 
 export default Home;
