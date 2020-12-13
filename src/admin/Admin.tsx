dabd113963c645cf1d1ead317b2d8f69687aacb3
Alex Wohlbruck <alexwohlbruck@gmail.com>
2020-12-12 19:06:01 -0500

    Add profile photos

--- src/admin/Admin.tsx
+++ src/admin/Admin.tsx
@@ -71,7 +71,7 @@ function Admin() {
                                                         <td className="px-6 py-4 whitespace-nowrap">
                                                             <div className="flex items-center">
                                                                 <div className="flex-shrink-0 h-10 w-10">
-                                                                    <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="" />
+                                                                    <img className="h-10 w-10 rounded-full" src={beeper.photoUrl} alt="" />
                                                                 </div>
                                                                 <div className="ml-4">
                                                                     <div className="text-sm font-medium text-gray-900">
@@ -84,13 +84,13 @@ function Admin() {
                                                             </div>
                                                         </td>
                                                         <td className="px-6 py-4 whitespace-nowrap">
-                                                            <div className="text-sm text-gray-900">{beeper.queueSize}</div>
+                                                            <div className="text-sm text-gray-900">{beeper.queueSize} riders</div>
                                                         </td>
                                                         <td className="px-6 py-4 whitespace-nowrap">
-                                                            <div className="text-sm text-gray-900">{beeper.capacity}</div>
+                                                            <div className="text-sm text-gray-900">{beeper.capacity} riders</div>
                                                         </td>
                                                         <td className="px-6 py-4 whitespace-nowrap">
-                                                            <div className="text-sm text-gray-900">{beeper.singlesRate} / {beeper.groupRate}</div>
+                                                            <div className="text-sm text-gray-900">${beeper.singlesRate} / ${beeper.groupRate}</div>
                                                         </td>
                                                         <td className="px-6 py-4 whitespace-nowrap">
                                                             <div className="text-sm text-gray-900">{beeper.masksRequired ? 'Yes' : 'No'}</div>
