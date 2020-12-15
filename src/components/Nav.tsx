f86954354a9f5d77e5dba04dfda9e308c2d80462
Alex Wohlbruck <alexwohlbruck@gmail.com>
2020-12-15 00:13:39 -0500

    Include header title in nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 
 export function VerticalNav(props) {
-    return <ul>
-        {props.children}
-    </ul>
+    return <div className="px-6 mr-6">
+        <h2 className="mx-2 font-bold leading-7 text-gray-800 sm:truncate">
+            {props.title}
+        </h2>
+
+        <hr></hr>
+
+        <ul>
+            {props.children}
+        </ul>
+    </div>
 }
 
 export function VerticalNavItem(props) {
