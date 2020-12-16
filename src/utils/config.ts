58fe8056efd663b017fbb4882e729f2c7a7fc8b2
Banks Nussman <banks@nussman.us>
2020-12-15 22:14:40 -0500

    use /v1 insted of /api for all API calls

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-    apiUrl: (process.env.NODE_ENV === 'production' ? 'https://ridebeep.app/api' : 'https://dev.ridebeep.app/v1'),
+    apiUrl: (process.env.NODE_ENV === 'production' ? 'https://ridebeep.app/v1' : 'https://dev.ridebeep.app/v1'),
     baseUrl: (process.env.NODE_ENV === 'production' ? 'https://ridebeep.app/' : 'https://dev.ridebeep.app/')
 };
