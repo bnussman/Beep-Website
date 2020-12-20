feb914923c065de41e3df56381fa05d9622b40e5
Banks Nussman <banks@nussman.us>
2020-12-20 16:47:30 -0500

    added handled column

--- src/routes/admin/reports/index.tsx
+++ src/routes/admin/reports/index.tsx
@@ -26,6 +26,10 @@ function Reports() {
         fetchReports();
     }, []);
 
+    //yeah I should make this a component
+    const Yes = () => <div className="rounded-full bg-green-500 h-3 shadow w-3 shadow flex items-center justify-center..."></div>;
+    const No = () => <div className="rounded-full bg-red-500 h-3 shadow w-3 shadow flex items-center justify-center..."> </div>;
+
     return <>
         <Heading3>Reports</Heading3>
 
@@ -36,6 +40,7 @@ function Reports() {
                     <TH>Reported User</TH>
                     <TH>Reason</TH>
                     <TH>Date</TH>
+                    <TH>Handled?</TH>
                     <TH></TH>
                 </THead>
                 <TBody>
@@ -46,6 +51,7 @@ function Reports() {
                                 <TDText><NavLink to={`users/${reportEntry.reported.id}`}>{reportEntry.reported.first} {reportEntry.reported.last}</NavLink></TDText>
                                 <TDText>{reportEntry.report.reason}</TDText>
                                 <TDText>{dayjs().to(reportEntry.report.timestamp)}</TDText>
+                                <TDText>{reportEntry.report.handled ? <Yes/> : <No/>}</TDText>
                                 <TDButton to={`reports/${reportEntry.report.id}`}>View</TDButton>
                             </TR>
                         )
