7f7ad1318defedf7fce28e13d19d838d958d160d
Banks Nussman <banks@nussman.us>
2021-02-05 15:01:35 -0500

    fixed small things

--- src/types/Report.ts
+++ src/types/Report.ts
@@ -3,13 +3,11 @@ import { User } from './User';
 export interface Report {
     id: string;
     reason: string;
-    reportedId: string;
     reported: User;
-    reporterId: string;
     reporter: User;
     handledBy: User | null;
     timestamp: number;
     notes: string | null;
     handled: boolean;
-    beepEventId: string | undefined | null;
+    beep: any;
 }
